93e4a3e613a35361d12402067f0c4dc0118df241
Abdalrahman Al Hossieny Abdalmoniem <Abdalrahmanhossiney94@gmail.com>
2025-01-07 20:47:09 +0200

    Add files via upload

--- app.cpp
+++ app.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-   cout << "====================================\n";
-   cout << "== Calculate Your Age Application ==\n";
-   cout << "====================================\n";
-   
-   int age;
-   cin >> age;
 
-   int age_in_days = age * 365;
-   int age_in_hours = age_in_days * 24;
-  
-   cout << " Age in Days Is: " << age_in_days << " Days\n";
-   cout << " Age in Hours Is: " << age_in_hours << " Hours\n";
+int main(){
+int kilobytes;
+cout << "Enter the number of kilobytes:";
+cin >> kilobytes;
+
+int bytes = kilobytes * 1024;
+int bits = bytes * 8;
+
+cout << kilobytes << " kilobytes is equal to " << bytes << " bytes and " << bits << " bits." << endl;
 
   return 0;
 }
